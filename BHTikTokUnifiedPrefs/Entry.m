5dae6212da3d8ffd9117ac0fd7714a128c98db33
sgyz520 <60571750+sgyz520@users.noreply.github.com>
2025-09-11 19:36:33 +0800

    Update Entry.m

--- BHTikTokUnifiedPrefs/Entry.m
+++ BHTikTokUnifiedPrefs/Entry.m
@@ -13,103 +13,150 @@
         NSMutableArray *specifiers = [NSMutableArray array];
         
         // 添加标题组
-        PSSpecifier *titleGroup = [[PSSpecifier alloc] init];
-        [titleGroup setName:@"BHTikTok Unified"];
+        PSSpecifier *titleGroup = [PSSpecifier preferenceSpecifierNamed:@"BHTikTok Unified" 
+                                                               target:self 
+                                                                  set:nil 
+                                                                  get:nil 
+                                                               detail:nil 
+                                                                  cell:PSGroupCell 
+                                                                  edit:nil];
         [specifiers addObject:titleGroup];
         
         // 添加版本信息
-        PSSpecifier *versionSpec = [[PSSpecifier alloc] init];
-        [versionSpec setName:@"Version"];
-        [versionSpec setIdentifier:@"version"];
+        PSSpecifier *versionSpec = [PSSpecifier preferenceSpecifierNamed:@"Version" 
+                                                               target:self 
+                                                                  set:nil 
+                                                                  get:nil 
+                                                               detail:nil 
+                                                                  cell:PSStaticTextCell 
+                                                                  edit:nil];
         [versionSpec setProperty:@"25.9.10" forKey:@"default"];
+        [versionSpec setProperty:@"version" forKey:@"key"];
         [specifiers addObject:versionSpec];
         
         // 添加作者信息
-        PSSpecifier *authorSpec = [[PSSpecifier alloc] init];
-        [authorSpec setName:@"Author"];
-        [authorSpec setIdentifier:@"author"];
+        PSSpecifier *authorSpec = [PSSpecifier preferenceSpecifierNamed:@"Author" 
+                                                               target:self 
+                                                                  set:nil 
+                                                                  get:nil 
+                                                               detail:nil 
+                                                                  cell:PSStaticTextCell 
+                                                                  edit:nil];
         [authorSpec setProperty:@"BHTikTok Unified Team" forKey:@"default"];
+        [authorSpec setProperty:@"author" forKey:@"key"];
         [specifiers addObject:authorSpec];
         
         // 添加功能设置组
-        PSSpecifier *settingsGroup = [[PSSpecifier alloc] init];
-        [settingsGroup setName:@"功能设置"];
+        PSSpecifier *settingsGroup = [PSSpecifier preferenceSpecifierNamed:@"功能设置" 
+                                                                  target:self 
+                                                                     set:nil 
+                                                                     get:nil 
+                                                                  detail:nil 
+                                                                     cell:PSGroupCell 
+                                                                     edit:nil];
         [specifiers addObject:settingsGroup];
         
         // 添加下载功能开关
-        PSSpecifier *downloadSwitch = [[PSSpecifier alloc] init];
-        [downloadSwitch setName:@"启用下载功能"];
-        [downloadSwitch setIdentifier:@"enableDownload"];
+        PSSpecifier *downloadSwitch = [PSSpecifier preferenceSpecifierNamed:@"启用下载功能" 
+                                                                   target:self 
+                                                                      set:@selector(setPreferenceValue:specifier:) 
+                                                                      get:@selector(readPreferenceValue:) 
+                                                                   detail:nil 
+                                                                      cell:PSSwitchCell 
+                                                                      edit:nil];
+        [downloadSwitch setProperty:@"enableDownload" forKey:@"key"];
         [downloadSwitch setProperty:@YES forKey:@"default"];
-        [downloadSwitch setTarget:self];
-        [downloadSwitch setSetAction:@selector(setPreferenceValue:forSpecifier:)];
-        [downloadSwitch setGetAction:@selector(readPreferenceValue:)];
         [specifiers addObject:downloadSwitch];
         
         // 添加地区信息显示开关
-        PSSpecifier *regionSwitch = [[PSSpecifier alloc] init];
-        [regionSwitch setName:@"显示地区信息"];
-        [regionSwitch setIdentifier:@"showRegionInfo"];
+        PSSpecifier *regionSwitch = [PSSpecifier preferenceSpecifierNamed:@"显示地区信息" 
+                                                                 target:self 
+                                                                    set:@selector(setPreferenceValue:specifier:) 
+                                                                    get:@selector(readPreferenceValue:) 
+                                                                 detail:nil 
+                                                                    cell:PSSwitchCell 
+                                                                    edit:nil];
+        [regionSwitch setProperty:@"showRegionInfo" forKey:@"key"];
         [regionSwitch setProperty:@YES forKey:@"default"];
-        [regionSwitch setTarget:self];
-        [regionSwitch setSetAction:@selector(setPreferenceValue:forSpecifier:)];
-        [regionSwitch setGetAction:@selector(readPreferenceValue:)];
         [specifiers addObject:regionSwitch];
         
         // 添加上传时间显示开关
-        PSSpecifier *uploadTimeSwitch = [[PSSpecifier alloc] init];
-        [uploadTimeSwitch setName:@"显示上传时间"];
-        [uploadTimeSwitch setIdentifier:@"showUploadTime"];
+        PSSpecifier *uploadTimeSwitch = [PSSpecifier preferenceSpecifierNamed:@"显示上传时间" 
+                                                                     target:self 
+                                                                        set:@selector(setPreferenceValue:specifier:) 
+                                                                        get:@selector(readPreferenceValue:) 
+                                                                     detail:nil 
+                                                                        cell:PSSwitchCell 
+                                                                        edit:nil];
+        [uploadTimeSwitch setProperty:@"showUploadTime" forKey:@"key"];
         [uploadTimeSwitch setProperty:@YES forKey:@"default"];
-        [uploadTimeSwitch setTarget:self];
-        [uploadTimeSwitch setSetAction:@selector(setPreferenceValue:forSpecifier:)];
-        [uploadTimeSwitch setGetAction:@selector(readPreferenceValue:)];
         [specifiers addObject:uploadTimeSwitch];
         
         // 添加复制功能组
-        PSSpecifier *copyGroup = [[PSSpecifier alloc] init];
-        [copyGroup setName:@"复制功能"];
+        PSSpecifier *copyGroup = [PSSpecifier preferenceSpecifierNamed:@"复制功能" 
+                                                               target:self 
+                                                                  set:nil 
+                                                                  get:nil 
+                                                               detail:nil 
+                                                                  cell:PSGroupCell 
+                                                                  edit:nil];
         [specifiers addObject:copyGroup];
         
         // 添加复制视频描述开关
-        PSSpecifier *copyDescSwitch = [[PSSpecifier alloc] init];
-        [copyDescSwitch setName:@"复制视频描述"];
-        [copyDescSwitch setIdentifier:@"copyVideoDecription"];
+        PSSpecifier *copyDescSwitch = [PSSpecifier preferenceSpecifierNamed:@"复制视频描述" 
+                                                                   target:self 
+                                                                      set:@selector(setPreferenceValue:specifier:) 
+                                                                      get:@selector(readPreferenceValue:) 
+                                                                   detail:nil 
+                                                                      cell:PSSwitchCell 
+                                                                      edit:nil];
+        [copyDescSwitch setProperty:@"copyVideoDecription" forKey:@"key"];
         [copyDescSwitch setProperty:@YES forKey:@"default"];
-        [copyDescSwitch setTarget:self];
-        [copyDescSwitch setSetAction:@selector(setPreferenceValue:forSpecifier:)];
-        [copyDescSwitch setGetAction:@selector(readPreferenceValue:)];
         [specifiers addObject:copyDescSwitch];
         
         // 添加复制视频链接开关
-        PSSpecifier *copyLinkSwitch = [[PSSpecifier alloc] init];
-        [copyLinkSwitch setName:@"复制视频链接"];
-        [copyLinkSwitch setIdentifier:@"copyVideoLink"];
+        PSSpecifier *copyLinkSwitch = [PSSpecifier preferenceSpecifierNamed:@"复制视频链接" 
+                                                                   target:self 
+                                                                      set:@selector(setPreferenceValue:specifier:) 
+                                                                      get:@selector(readPreferenceValue:) 
+                                                                   detail:nil 
+                                                                      cell:PSSwitchCell 
+                                                                      edit:nil];
+        [copyLinkSwitch setProperty:@"copyVideoLink" forKey:@"key"];
         [copyLinkSwitch setProperty:@YES forKey:@"default"];
-        [copyLinkSwitch setTarget:self];
-        [copyLinkSwitch setSetAction:@selector(setPreferenceValue:forSpecifier:)];
-        [copyLinkSwitch setGetAction:@selector(readPreferenceValue:)];
         [specifiers addObject:copyLinkSwitch];
         
         // 添加复制音乐链接开关
-        PSSpecifier *copyMusicSwitch = [[PSSpecifier alloc] init];
-        [copyMusicSwitch setName:@"复制音乐链接"];
-        [copyMusicSwitch setIdentifier:@"copyMusicLink"];
+        PSSpecifier *copyMusicSwitch = [PSSpecifier preferenceSpecifierNamed:@"复制音乐链接" 
+                                                                    target:self 
+                                                                       set:@selector(setPreferenceValue:specifier:) 
+                                                                       get:@selector(readPreferenceValue:) 
+                                                                    detail:nil 
+                                                                       cell:PSSwitchCell 
+                                                                       edit:nil];
+        [copyMusicSwitch setProperty:@"copyMusicLink" forKey:@"key"];
         [copyMusicSwitch setProperty:@YES forKey:@"default"];
-        [copyMusicSwitch setTarget:self];
-        [copyMusicSwitch setSetAction:@selector(setPreferenceValue:forSpecifier:)];
-        [copyMusicSwitch setGetAction:@selector(readPreferenceValue:)];
         [specifiers addObject:copyMusicSwitch];
         
         // 添加关于组
-        PSSpecifier *aboutGroup = [[PSSpecifier alloc] init];
-        [aboutGroup setName:@"关于"];
+        PSSpecifier *aboutGroup = [PSSpecifier preferenceSpecifierNamed:@"关于" 
+                                                               target:self 
+                                                                  set:nil 
+                                                                  get:nil 
+                                                               detail:nil 
+                                                                  cell:PSGroupCell 
+                                                                  edit:nil];
         [specifiers addObject:aboutGroup];
         
         // 添加关于按钮
-        PSSpecifier *aboutButton = [[PSSpecifier alloc] init];
-        [aboutButton setName:@"关于"];
-        [aboutButton setIdentifier:@"about"];
+        PSSpecifier *aboutButton = [PSSpecifier preferenceSpecifierNamed:@"关于" 
+                                                                target:self 
+                                                                   set:nil 
+                                                                   get:nil 
+                                                                detail:nil 
+                                                                   cell:PSButtonCell 
+                                                                   edit:nil];
+        [aboutButton setProperty:@"about" forKey:@"key"];
         [aboutButton setButtonAction:@selector(showAbout)];
         [specifiers addObject:aboutButton];
         
@@ -120,8 +167,8 @@
 }
 
 - (id)readPreferenceValue:(PSSpecifier*)specifier {
-    NSString *key = [specifier identifier];
-    id defaultValue = [specifier properties][@"default"];
+    NSString *key = [specifier propertyForKey:@"key"];
+    id defaultValue = [specifier propertyForKey:@"default"];
     
     // 从BHIManager获取设置值
     if ([key isEqualToString:@"enableDownload"]) {
@@ -146,7 +193,7 @@
 }
 
 - (void)setPreferenceValue:(id)value forSpecifier:(PSSpecifier*)specifier {
-    NSString *key = [specifier identifier];
+    NSString *key = [specifier propertyForKey:@"key"];
     
     // 保存设置值到NSUserDefaults
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
@@ -158,12 +205,11 @@
 }
 
 - (void)showAbout {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"关于 BHTikTok Unified"
-                                                    message:@"功能强大的 TikTok 增强插件，整合版本结合完整功能实现与多语言支持。\n\n基于 @hahios-2506 的 BHTiktok 和 @dayanch96 的 BHTikTok-Plus 项目"
-                                                   delegate:nil
-                                          cancelButtonTitle:@"确定"
-                                          otherButtonTitles:nil];
-    [alert show];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"关于 BHTikTok Unified"
+                                                                   message:@"功能强大的 TikTok 增强插件，整合版本结合完整功能实现与多语言支持。\n\n基于 @hahios-2506 的 BHTiktok 和 @dayanch96 的 BHTikTok-Plus 项目"
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
